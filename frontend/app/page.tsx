6ae5e868005ecd06f657bb3f0a12870588e648c4
Kazim <mkazimraza106@gmail.com>
2026-05-11 01:20:35 +0500

    solving error in vercel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -5,19 +5,25 @@ import Pricing from "@/components/home/Pricing";
 import CTA from "@/components/home/CTA";
 import FAQs from "@/components/home/FAQs";
 import ProductPreview from "@/components/home/ProductPreview";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
-    <main>
-      <Hero />
-      <hr className="center-section text-[#ccc]" />
-      <ProductPreview />
-      <Features />
-      <HowItWorks />
-      <hr className="center-section text-[#ccc]" />
-      <Pricing />
-      <FAQs />
-      <CTA />
-    </main>
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main>
+        <Hero />
+        <hr className="center-section text-[#ccc]" />
+        <ProductPreview />
+        <Features />
+        <HowItWorks />
+        <hr className="center-section text-[#ccc]" />
+        <Pricing />
+        <FAQs />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
   );
 }
