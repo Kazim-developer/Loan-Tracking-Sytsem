71f59e16ac1e6d907a01789569d3c4f02c0ad30c
Kazim <mkazimraza106@gmail.com>
2026-05-11 01:23:11 +0500

    testing vercel errors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,17 +1,18 @@
-import Features from "@/components/home/Features";
-import Hero from "@/components/home/Hero";
-import HowItWorks from "@/components/home/HowItWorks";
-import Pricing from "@/components/home/Pricing";
-import CTA from "@/components/home/CTA";
-import FAQs from "@/components/home/FAQs";
-import ProductPreview from "@/components/home/ProductPreview";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+// import Features from "@/components/home/Features";
+// import Hero from "@/components/home/Hero";
+// import HowItWorks from "@/components/home/HowItWorks";
+// import Pricing from "@/components/home/Pricing";
+// import CTA from "@/components/home/CTA";
+// import FAQs from "@/components/home/FAQs";
+// import ProductPreview from "@/components/home/ProductPreview";
+// import Navbar from "@/components/Navbar";
+// import Footer from "@/components/Footer";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar />
+      <h1>Home page</h1>
+      {/* <Navbar />
       <main>
         <Hero />
         <hr className="center-section text-[#ccc]" />
@@ -23,7 +24,7 @@ export default function HomePage() {
         <FAQs />
         <CTA />
       </main>
-      <Footer />
+      <Footer /> */}
     </div>
   );
 }
